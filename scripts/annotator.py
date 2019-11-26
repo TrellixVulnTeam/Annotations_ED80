e567df3db9783eac9526f0576c013a2d26c4507d
Eoin <daly.e@husky.neu.edu>
2019-11-26 10:24:04 -0500

    updated scripts
    added finding the time in the json

--- scripts/annotator.py
+++ scripts/annotator.py
@@ -4,6 +4,9 @@ import argparse
 import json
 import rosbag
 
+path = os.getcwd()
+json_path = path + "/jsons"
+
 text = 'This is a test program. It demonstrates how to use the argparse module with a program description.'
 
 parser = argparse.ArgumentParser(description = text)
@@ -22,15 +25,19 @@ def initVars():
     end = args.end
      
     bagFile = str(args.input)
-    bag = rosbag.Bag(bagFile)
     if not bagFile.endswith(".bag"):
         raise NameError("Input file should be a .bag file")
+    bag = "bag"#rosbag.Bag(bagFile)
 
-    jsonFile = args.annotation
-    if not jsonFile.endswith(".json"):
+
+    f = args.annotation
+    if not f.endswith(".json"):
         raise NameError("Annotation file should be a .json file")
+    f_path = json_path + "/" + f
+    jsonFile = open(f_path, 'r')
+    jDict = json.load(jsonFile)
 
-    return topic, start, end, bag, jsonFile
+    return topic, start, end, bag, jDict
 
 # prints out all available topics of the given bag file
 def findTopics(bag):
@@ -40,9 +47,29 @@ def findTopics(bag):
         types.append(bag.get_type_and_topic_info()[1].values()[i][0])
     print(types)
 
-if __name__ == "__main__":
-    set_topic, start_tag, end_tag, bag, jFile = initVars()
+# returns lists of the times of starts and ends
+def find_times(start, end, json_dict):
+    start_times = []
+    end_times = []
+    keys = sorted(list(json_dict.keys()))
+    for k in keys:
+        entry = json_dict[k]
+        if entry["key"] == start:
+            start_times.append(entry.get("rel_time_sec"))
+        if entry["key"] == end:
+            end_times.append(entry.get("rel_time_sec"))
     
-    findTopics(bag)
+    return [start_times, end_times]
+
+
+if __name__ == "__main__":
+    set_topic, start_tag, end_tag, bag, json_dict = initVars()
+    
+    #findTopics(bag)
+    [start_times, end_times] = find_times(start_tag, end_tag, json_dict)
+    
+
+
+
 
     bag.close()   
